094fbce13cc4d1439557368e8ccce53b6690d318
huan-chin chen <cherry032826@gmail.com>
2024-06-02 15:43:46 +0800

    connect to mongoDB and insert data

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 import crypto from "crypto";
+// import AWS from "aws-sdk";
+import mongoose from "mongoose";
+import Url from "./urlModel.js";
+import User from "./userModel.js";
+
+let isConnected;
+
+const connectToDatabase = async () => {
+  if (isConnected) {
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    isConnected = mongoose.connection.readyState;
+    console.log("DB connection successful!");
+  } catch (error) {
+    console.error("Error connecting to database:", error);
+  }
+};
 
 function md5ToBase62(input) {
   const md5Hash = crypto.createHash("md5").update(input).digest("hex");
@@ -17,23 +40,78 @@ function md5ToBase62(input) {
   return base62Str;
 }
 
-exports.handler = async (event) => {
+export const handler = async (event) => {
   try {
-    const { url } = JSON.parse(event.body);
-    const { hostname, pathname, search } = new URL(url);
-    const shortHostname = md5ToBase62(hostname);
-    const shortPathname = md5ToBase62(pathname);
-    const shortSearch = md5ToBase62(search);
-    const shortUrl = `https://${shortHostname}/${shortPathname}/${shortSearch}`;
+    await connectToDatabase();
+
+    if (!event.body) {
+      throw new Error("Event body is undefined");
+    }
+
+    const { url } = event.body;
+
+    if (!url) {
+      throw new Error("URL is missing from the input");
+    }
+
+    const shortUrl = md5ToBase62(url).slice(0, 8);
+    const finalUrl = `https://${shortUrl}`;
+
+    const payload = {
+      userId: "665adb6426688cd8f9f914c3",
+      shorturl: finalUrl,
+      longurl: url,
+    };
+
+    const result = await Url.create(payload);
+    console.log(result);
+
+    // const params = {
+    //   FunctionName: "connectMongo",
+    //   InvocationType: "Event", // 使用事件调用，异步执行
+    //   Payload: JSON.stringify(payload),
+    // };
+
+    // 尝试调用 Lambda 函数
+    // const invokeResponse = await lambda.invoke(params).promise();
+
+    // if (invokeResponse.StatusCode === 200) {
+    //   console.log("Successfully invoked the Lambda function");
+    // } else {
+    //   throw new Error(invokeResponse.FunctionError);
+    // }
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ shortUrl }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: payload,
     };
   } catch (error) {
+    // 捕获并处理异常
+    console.error("Error invoking the Lambda function:", error);
+
+    // 打印错误消息
+    console.error("Error message:", error.message);
+
+    // 如果有堆栈跟踪，打印堆栈跟踪信息
+    if (error.stack) {
+      console.error("Stack trace:", error.stack);
+    }
+
     return {
       statusCode: 400,
-      body: JSON.stringify({ error: error.message }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: { error: error.message },
     };
   }
 };
+
+handler();
---
+++ urlModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const urlSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  longurl: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  shorturl: {
+    type: String,
+    required: true,
+  },
+  numOfClicks: {
+    type: Number,
+    default: 0,
+  },
+  lastUpdate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Url = mongoose.model("url", urlSchema);
+
+export default Url;
